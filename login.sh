1111e80f35aee9176de14daaecae5c8d620daefe
jasper villarosa <jasper.villarosa@awsys-i.com>
2024-04-05 17:54:40 +0800

    Added additional lessons that were left

---
+++ login.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+case ${1,,} in 
+	jasper | administrator ) 
+		echo "Hello, you are the boss here!"
+		;;
+	help)
+		echo "Just enter your username"
+		;;
+	*)
+		echo "Hello there. You are not the boss of me. Enter a valid username"
+	esac
+	
+
